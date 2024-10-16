d78977201fdb309ab5554f2ca55daf22863aa574
Mahesh77r <shravanrohane19@gmail.com>
2024-10-16 11:51:37 +0530

    Push Notification

--- App.js
+++ App.js
@@ -22,8 +22,6 @@ export default function App() {
         authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
         authStatus === messaging.AuthorizationStatus.PROVISIONAL;
   
-
-        
       if (enabled) {
         getFcmToken();
       }
